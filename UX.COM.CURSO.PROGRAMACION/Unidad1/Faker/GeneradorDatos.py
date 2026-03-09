8f95125411f2710a3151ff956865acfcdcebebf7
Rodrigo <rodrigo8a13@gmail.com>
2026-03-09 07:43:23 -0600

    Ejercicio Faker

---
+++ UX.COM.CURSO.PROGRAMACION/Unidad1/Faker/GeneradorDatos.py
@@ -0,0 +1,8 @@
+#importar libreria faker
+from faker import Faker
+faker = Faker("es_MX")
+print("Generando datos Dumpy con Faker")
+print(f"Nombre:{faker.name()}")
+print(f"Direccion:{faker.address()}")
+print(f"Telefono:{faker.phone_number()}")
+print(f"Correo:{faker.email()}")
