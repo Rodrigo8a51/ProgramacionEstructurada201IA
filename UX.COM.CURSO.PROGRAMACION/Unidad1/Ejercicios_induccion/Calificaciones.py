3a5c3c01ce40c23185853c837361e7ae970b9903
Rodrigo <rodrigo8a13@gmail.com>
2026-03-13 08:18:08 -0600

    Ejercicio cadena

---
+++ UX.COM.CURSO.PROGRAMACION/Unidad1/Ejercicios_induccion/Calificaciones.py
@@ -0,0 +1,20 @@
+def calificaciones():
+    calificacion = input("Ingresar numero de calificacion")
+    if calificacion > 100:
+        print("Ingrese un numero valido")
+    elif calificacion >= 90:
+        print("Calificacion: A")
+    elif calificacion >= 80:
+        print("Calificacion: B")
+    elif calificacion >= 70:
+        print("Calificacion: C")
+    elif calificacion >= 69:
+        print("Calificacion: D")
+    elif calificacion < 69:
+        print("Calificacion: F")
+            
+def main():
+    calificaciones()
+
+if __name__=="__main__":
+    main()
---
+++ UX.COM.CURSO.PROGRAMACION/Unidad1/Manejo_cadena/Ejercicio_cadena.py
@@ -0,0 +1,12 @@
+def demostracion_cadena():
+    cadena = "Universidad de Xalapa"
+    print(cadena.upper())
+    print(cadena.lower())
+    print(cadena.capitalize())
+    print(cadena.title())
+
+def main():
+    demostracion_cadena()
+
+if __name__=="__main__":
+    main()
