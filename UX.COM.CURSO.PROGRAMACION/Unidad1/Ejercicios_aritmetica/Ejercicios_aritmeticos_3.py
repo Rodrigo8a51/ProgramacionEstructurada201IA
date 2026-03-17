553afb9afd9372fc3e95ce643a6c529f241cafa8
Rodrigo <rodrigo8a13@gmail.com>
2026-03-17 08:07:26 -0600

    Ejercicio Tangente

--- UX.COM.CURSO.PROGRAMACION/Unidad1/Ejercicios_aritmetica/Ejercicios_aritmeticos_3.py
+++ UX.COM.CURSO.PROGRAMACION/Unidad1/Ejercicios_aritmetica/Ejercicios_aritmeticos_3.py
@@ -12,9 +12,22 @@ def mostrar_funciones_math(numero):
 
     print("El resultado de sen^2(x) + cos^2(x) es", resultado)
 
+
+def mostrar_tan_math(numero):
+    # TANGENTE DEL NUMERO
+    tan_x = math.tan(numero)
+    # IMPRIMIR LA TANGENTE
+    print("La Tangente de", numero, "es", tan_x)
+
+    # RESULTADO DE LA OPERACION PERO CON TANGENTE
+    resultado_tan =  tan_x ** 2
+    print("El resultado de tan^2(x) ", resultado_tan)
+
+
 def main():
-    numero = float (input ("Ingresa un numero"))
+    numero = float (input ("Ingresa un numero "))
     mostrar_funciones_math(numero)
+    mostrar_tan_math(numero)
 
 if __name__=="__main__":
     main()
