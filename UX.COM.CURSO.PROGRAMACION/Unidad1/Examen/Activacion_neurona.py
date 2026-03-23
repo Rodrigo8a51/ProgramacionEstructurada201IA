230ed078eda07cf41bb0df97bf550aa0ac0b57f0
Rodrigo <rodrigo8a13@gmail.com>
2026-03-23 08:11:47 -0600

    Ejercicio Examen

--- UX.COM.CURSO.PROGRAMACION/Unidad1/Examen/Activacion_neurona.py
+++ UX.COM.CURSO.PROGRAMACION/Unidad1/Examen/Activacion_neurona.py
@@ -1,6 +1,13 @@
-w = float(input("Ingrese el peso de la entrada: "))
-x = float(input("Ingrese el valor de la entrada: "))
+def activacion_neurona():
+    w = float(input("Ingrese el peso de la entrada: "))
+    x = float(input("Ingrese el dato de la entrada: "))
 
-# Z es el valor de activación de la neurona
-z = w * x
-print("El valor de activacion de la neurona es:", z)
+    # Z es el valor de activación de la neurona
+    z = w * x
+    print("El valor de activacion de la neurona es:", z)
+
+def main():
+    activacion_neurona()
+
+if __name__=="__main__":
+    main()
