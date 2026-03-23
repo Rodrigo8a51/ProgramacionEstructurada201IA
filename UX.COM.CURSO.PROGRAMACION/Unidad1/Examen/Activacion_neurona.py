5c18f47e56f82f49c649565919788b6e20ffccdb
Rodrigo <rodrigo8a13@gmail.com>
2026-03-23 07:51:13 -0600

    Ejercicio Examen

---
+++ UX.COM.CURSO.PROGRAMACION/Unidad1/Examen/Activacion_neurona.py
@@ -0,0 +1,6 @@
+w = float(input("Ingrese el peso de la entrada: "))
+x = float(input("Ingrese el valor de la entrada: "))
+
+# Z es el valor de activación de la neurona
+z = w * x
+print("El valor de activacion de la neurona es:", z)
