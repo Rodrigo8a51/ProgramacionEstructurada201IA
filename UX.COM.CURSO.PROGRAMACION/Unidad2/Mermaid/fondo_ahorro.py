cfd73f904922b33900c29394bc2122462e10a937
Rodrigo <rodrigo8a13@gmail.com>
2026-04-07 07:55:50 -0600

    Ejercicio Mermaid

--- UX.COM.CURSO.PROGRAMACION/Unidad2/Mermaid/fondo_ahorro.py
+++ UX.COM.CURSO.PROGRAMACION/Unidad2/Mermaid/fondo_ahorro.py
@@ -5,13 +5,13 @@ def fondo():
     saldo= 0
     meta = 1000
     while saldo < meta:
-        deposito = int(input("Ingrese deposito: "))
+        deposito = float(input("Ingrese deposito: "))
         saldo += deposito
     return saldo
 
 def main():
     resultado = fondo()
-    print("El fondo de ahorro es", resultado)
+    print("La meta ha sido superada", resultado)
 
 if __name__ == "__main__":
     main()
