8f9524bbe195b77bc64490c2f8d7d9cd0c93ad6a
Rodrigo <rodrigo8a13@gmail.com>
2026-04-07 20:43:41 -0600

    a3

--- UX.COM.CURSO.PROGRAMACION/Unidad2/asistente_logico.py
+++ UX.COM.CURSO.PROGRAMACION/Unidad2/asistente_logico.py
@@ -3,8 +3,13 @@ def asistente():
     print("Hola, soy", nombre_asistente, " tu asistente")
 
     dato = input("¿En qué puedo ayudarte? ").lower()
+
     if dato == "hola" or "buenos dias":
         print("¡Hola! Soy tu asistente. Es un gusto saludarte.")
+
+    elif dato == "clima" or "temperatura":
+         print("Consultando el servicio meteorológico... Hoy en Xalapa tendremos un día nublado.")
+    
     
 
 
