a3dcf8fa0de52a45a5b50d10c40e4a1daad309af
Rodrigo <rodrigo8a13@gmail.com>
2026-04-07 20:40:11 -0600

    a2

--- UX.COM.CURSO.PROGRAMACION/Unidad2/asistente_logico.py
+++ UX.COM.CURSO.PROGRAMACION/Unidad2/asistente_logico.py
@@ -1,6 +1,12 @@
 def asistente():
     nombre_asistente = "IA-N"
-    print("Hola, soy", nombre_asistente, " tu asistente.")
+    print("Hola, soy", nombre_asistente, " tu asistente")
+
+    dato = input("¿En qué puedo ayudarte? ").lower()
+    if dato == "hola" or "buenos dias":
+        print("¡Hola! Soy tu asistente. Es un gusto saludarte.")
+    
+
 
 def main():
     asistente()
