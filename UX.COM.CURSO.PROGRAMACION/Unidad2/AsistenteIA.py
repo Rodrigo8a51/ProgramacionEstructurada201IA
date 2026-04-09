b1b71377d2b4bd51f361062eabbe0d4c42c61735
Rodrigo <rodrigo8a13@gmail.com>
2026-04-08 22:46:36 -0600

    a2

---
+++ UX.COM.CURSO.PROGRAMACION/Unidad2/AsistenteIA.py
@@ -0,0 +1,8 @@
+
+def asistente_ia():
+    UMBRAL_ALTO = 80.0
+    UMBRAL_MINIMO = 40.0
+    
+    instrucción_detectada = input("Instrucción recibida: ")
+    nivel_confianza = float(input("Nivel de confianza calculado (%): "))
+
