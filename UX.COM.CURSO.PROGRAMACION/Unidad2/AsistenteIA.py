9c2a83c2effb2f364ee6f9508f542dcc3a40798a
Rodrigo <rodrigo8a13@gmail.com>
2026-04-08 22:50:23 -0600

    a3

--- UX.COM.CURSO.PROGRAMACION/Unidad2/AsistenteIA.py
+++ UX.COM.CURSO.PROGRAMACION/Unidad2/AsistenteIA.py
@@ -2,7 +2,15 @@
 def asistente_ia():
     UMBRAL_ALTO = 80.0
     UMBRAL_MINIMO = 40.0
-    
+
     instrucción_detectada = input("Instrucción recibida: ")
     nivel_confianza = float(input("Nivel de confianza calculado (%): "))
 
+    if nivel_confianza >= UMBRAL_ALTO:
+        print("Ejecutando la acción:" + instrucción_detectada + "... (Éxito)")
+
+def main():
+    asistente_ia()
+    
+if __name__ == "__main__":    
+    main()
