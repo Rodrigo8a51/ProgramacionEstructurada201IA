ded9e77c53753a7a8885f86288f6aac43e43de8a
Rodrigo <rodrigo8a13@gmail.com>
2026-04-13 19:16:39 -0600

    Ejercicio Piloto Automatico

---
+++ UX.COM.CURSO.PROGRAMACION/Unidad3/piloto_automatico.py
@@ -0,0 +1,11 @@
+def piloto_automatico():
+    objeto_cercano = float(input("¿A qué distancia está el objeto más cercano (en metros)?: "))
+    color_semaforo = input("¿De qué color está el semáforo? (verde/amarillo/rojo): ").lower()
+    peaton = input("¿Hay un peatón cruzando? (si/no): ").lower()
+
+def main():
+    piloto_automatico()
+
+if __name__ == "__main__":
+    main()
+    
