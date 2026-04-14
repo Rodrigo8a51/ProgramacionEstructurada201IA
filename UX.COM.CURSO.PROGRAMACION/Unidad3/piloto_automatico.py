f143c9abe651f6e1d9c2dd1de8c5c0c3eeb8d9db
Rodrigo <rodrigo8a13@gmail.com>
2026-04-13 19:22:32 -0600

    a3

--- UX.COM.CURSO.PROGRAMACION/Unidad3/piloto_automatico.py
+++ UX.COM.CURSO.PROGRAMACION/Unidad3/piloto_automatico.py
@@ -6,7 +6,16 @@ def piloto_automatico():
     if distancia < 5 or peaton == "si":
         print("¡FRENO DE EMERGENCIA ACTIVADO! Deteniendo el vehículo inmediatamente.")
 
+    elif color_semaforo == "rojo":
+        print("Estado: Detenido. Esperando luz verde.")
+    
+    elif color_semaforo == "amarillo":
+        print("Estado: Precaución. Preparándose para detenerse.")
+    
+    elif color_semaforo == "verde" and distancia >= 5:
+        print("Estado: En movimiento. Todo despejado para avanzar.")
 
+   
 
 def main():
     piloto_automatico()
