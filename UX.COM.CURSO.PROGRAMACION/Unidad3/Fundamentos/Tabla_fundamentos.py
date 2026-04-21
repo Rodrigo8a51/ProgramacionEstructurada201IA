3754c7929ce6733f4b4e9fc249099cba43a9aaa6
Rodrigo <rodrigo8a13@gmail.com>
2026-04-21 08:49:49 -0600

    Ejercicios Logica Tabla

--- UX.COM.CURSO.PROGRAMACION/Unidad3/Fundamentos/Tabla_fundamentos.py
+++ UX.COM.CURSO.PROGRAMACION/Unidad3/Fundamentos/Tabla_fundamentos.py
@@ -5,18 +5,18 @@ def tabla_multiplicar(n):
         print(f"{i:4}", end="")
     print()
     
-    print("    " + "----" * n)  # Línea separadora
+    print("    " + "----" * n) 
     
-    # Filas de la tabla
+
     for fila in range(1, n+1):
-        print(f"{fila:2} *", end="")  # Etiqueta de fila
+        print(f"{fila:2} *", end="")  
         for col in range(1, n+1):
             print(f"{fila*col:4}", end="")
-        print()  # Salto de línea al terminar cada fila
+        print() 
 
 
 def main():
-    # Pedir al usuario hasta qué número quiere la tabla
+
     limite = int(input("¿Hasta qué número quieres la tabla de multiplicar? "))
     tabla_multiplicar(limite)
 
