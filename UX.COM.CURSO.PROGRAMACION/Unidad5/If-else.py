c65f259b24c4df767fa59f7b5ed76b53780f454f
Rodrigo <rodrigo8a13@gmail.com>
2026-05-26 07:55:46 -0600

    If-Else v1: Identificar pares e impares

---
+++ UX.COM.CURSO.PROGRAMACION/Unidad5/If-else.py
@@ -0,0 +1,20 @@
+def odd_even (number):
+    if number % 2 == 1:
+        return "Weird"
+    else:
+        if 2 <= number < 5:
+            return "Not Weird"
+        elif 6 <= number <= 20:
+            return "Weird"
+        elif number > 20:
+            return "Not Weird"
+
+def main():
+    n = int(input("Ingrese un número entero: "))
+    result = odd_even(n)
+    print(result)
+    
+    
+
+if __name__ == "__main__":
+    main()
